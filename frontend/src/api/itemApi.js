63ab69b227fc3814c9c5712a24135aaa08b5882c
IT24103572 <IT24103572@my.sliit.lk>
2026-04-29 10:57:38 +0530

    UPDATEAPI

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://labtest-production-d631.up.railway.app",
+  baseURL: import.meta.env.VITE_API_URL || "https://labtest-production-d631.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
